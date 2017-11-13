3a114db57f91c28f6bbcbcbe34ab6f6c18f4aef0
Marek Dohnal <datgaspkitteh@gmail.com>
2017-11-13 22:43:24 +0100

    Done how it should've been done.

--- Bjarne PPP - CH 4 Ex. 2/Bjarne PPP - CH 4 Ex.2.cpp
+++ Bjarne PPP - CH 4 Ex. 2/Bjarne PPP - CH 4 Ex.2.cpp
@@ -10,9 +10,20 @@ int main()
 	keep_window_open();
 
 	sort(temps);
-	double median = (temps[0] + temps[temps.size() - 1]) / 2;
-	cout << "The median of this vector is: " << median << '\n';
 
+	double even_median = (temps[(temps.size() - 1 ) / 2] + temps[(temps.size() / 2)]) / 2;
+	double odd_median = temps[temps.size() / 2 + 1];
+
+	if (temps.size() % 2 == 0)
+	{
+		cout << "The median of this vector is: " << even_median << '\n';
+	}
+	else
+	{
+		cout << "The median of this vector is: " << odd_median << '\n';
+	}
+	
+	
 	keep_window_open();
 }
 
